557aee537d29df02c2cab844643aeaeb1fa12b0b
simpleframework <cknet@126.com>
2013-12-06 18:16:17 +0800

    Signed-off-by: simpleframework <cknet@126.com>

---
+++ src/main/java/net/simpleframework/ctx/ModuleRefException.java
@@ -0,0 +1,26 @@
+package net.simpleframework.ctx;
+
+import net.simpleframework.common.th.RuntimeExceptionEx;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public class ModuleRefException extends RuntimeExceptionEx {
+
+	public ModuleRefException(final String msg, final Throwable cause) {
+		super(msg, cause);
+	}
+
+	public static ModuleRefException of(final String msg) {
+		return _of(ModuleRefException.class, msg);
+	}
+
+	public static ModuleRefException of(final Throwable throwable) {
+		return (ModuleRefException) _of(ModuleRefException.class, null, throwable);
+	}
+
+	private static final long serialVersionUID = -4892690738644084723L;
+}
