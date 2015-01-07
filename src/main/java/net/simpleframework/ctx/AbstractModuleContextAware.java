0827db5473e9d7f4335f10920c65e7e8a27cfb8d
simpleframework <cknet@126.com>
2015-01-07 17:00:06 +0800

    Signed-off-by: simpleframework <cknet@126.com>

---
+++ src/main/java/net/simpleframework/ctx/AbstractModuleContextAware.java
@@ -0,0 +1,39 @@
+package net.simpleframework.ctx;
+
+import java.lang.reflect.Field;
+
+import net.simpleframework.common.ClassUtils;
+import net.simpleframework.common.object.ObjectEx;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public class AbstractModuleContextAware extends ObjectEx {
+	protected IModuleContext _context;
+
+	public IModuleContext getModuleContext() {
+		if (_context == null && this instanceof IModuleContextAware) {
+			for (final Class<?> iClass : ClassUtils.getAllInterfaces(getClass())) {
+				if (IModuleContextAware.class.isAssignableFrom(iClass)) {
+					for (final Field field : iClass.getFields()) {
+						if (IModuleContext.class.isAssignableFrom(field.getType())) {
+							field.setAccessible(true);
+							try {
+								return (_context = (IModuleContext) field.get(this));
+							} catch (final IllegalAccessException e) {
+							}
+						}
+					}
+				}
+			}
+		}
+		return _context;
+	}
+
+	public void setModuleContext(final IModuleContext context) {
+		_context = context;
+	}
+}
--- src/main/java/net/simpleframework/ctx/service/AbstractBaseService.java
+++ src/main/java/net/simpleframework/ctx/service/AbstractBaseService.java
@@ -1,12 +1,7 @@
 package net.simpleframework.ctx.service;
 
-import java.lang.reflect.Field;
-
-import net.simpleframework.common.ClassUtils;
-import net.simpleframework.common.object.ObjectEx;
+import net.simpleframework.ctx.AbstractModuleContextAware;
 import net.simpleframework.ctx.IApplicationContext;
-import net.simpleframework.ctx.IModuleContext;
-import net.simpleframework.ctx.IModuleContextAware;
 import net.simpleframework.ctx.task.ITaskExecutor;
 
 /**
@@ -15,37 +10,11 @@ import net.simpleframework.ctx.task.ITaskExecutor;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public abstract class AbstractBaseService extends ObjectEx implements IBaseService {
-
-	private IModuleContext _context;
+public abstract class AbstractBaseService extends AbstractModuleContextAware implements
+		IBaseService {
 
 	@Override
 	public void onInit() throws Exception {
-	}
-
-	@Override
-	public IModuleContext getModuleContext() {
-		if (_context == null && this instanceof IModuleContextAware) {
-			for (final Class<?> iClass : ClassUtils.getAllInterfaces(getClass())) {
-				if (IModuleContextAware.class.isAssignableFrom(iClass)) {
-					for (final Field field : iClass.getFields()) {
-						if (IModuleContext.class.isAssignableFrom(field.getType())) {
-							field.setAccessible(true);
-							try {
-								return (_context = (IModuleContext) field.get(this));
-							} catch (final IllegalAccessException e) {
-							}
-						}
-					}
-				}
-			}
-		}
-		return _context;
-	}
-
-	@Override
-	public void setModuleContext(final IModuleContext context) {
-		_context = context;
 	}
 
 	protected IApplicationContext getApplicationContext() {
